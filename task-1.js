211b1f6f0ec25058210aa96497e25c912e97545c
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-14 23:40:36 +0600

    added task-1

---
+++ task-1.js
@@ -0,0 +1,10 @@
+/* 
+    Task-1
+
+    Take four parameters. Multiply the four numbers and then return the result
+*/
+
+function multiply(num1, num2, num3, num4) {
+  return num1 * num2 * num3 * num4;
+}
+console.log(multiply(2, 4, 6, 8));
